6dcb0badf302bfaa99761ae28af1c6cac54e209c
CeanAlagarCGA <alagar.cf@obmontessori.edu.ph>
2026-04-07 13:05:02 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+﻿function log_in() {
+    num1 = document.getElementById("input1").value;
+    num2 = document.getElementById("input2").value;
+
+    if (num1 === "Kuli_18" && num2 === "2026") {
+        window.alert("Login successful!");
+    } else {
+        window.alert("Incorrect username or password.");
+    }
+}
